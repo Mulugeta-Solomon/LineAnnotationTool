e91be360bf01f1e00eefbde583ae750106572c1e
dododoyo <dolphinmulugeta88@gmail.com>
2024-08-20 17:17:13 +0300

    🔄 Change Google SignIn Image

--- app/components/GoogleSignInButton.tsx
+++ app/components/GoogleSignInButton.tsx
@@ -17,12 +17,7 @@ const GoogleSignInButton = () => {
       className="border border-gray-300 font-medium py-4 px-8 rounded-xl flex items-center justify-center w-full hover:bg-blue-50 focus:outline-none focus:ring-2 mt-6"
     >
       <div className="h-5 w-5 mr-2">
-        <Image
-          src="https://w7.pngwing.com/pngs/344/344/png-transparent-google-logo-google-logo-g-suite-google-text-logo-symbol-thumbnail.png"
-          alt="Google logo"
-          width={20}
-          height={20}
-        />
+        <Image src="/google.svg" alt="Google logo" width={20} height={20} />
       </div>
       <span className="font-semibold">Sign In with Google</span>
     </button>
